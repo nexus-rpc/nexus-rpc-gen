ec2441b0ca55c4c693e48db04bde84a3627eed2b
Sam Mathis <sam.mathis@temporal.io>
2026-03-13 11:37:22 -0700

    Fix typo in console

--- src/packages/nexus-rpc-gen/src/index.ts
+++ src/packages/nexus-rpc-gen/src/index.ts
@@ -181,7 +181,7 @@ function printUsage() {
 // Node 22.18+ and 24.2+ provides `import.meta.main`, which deprecates the need for
 // this npm library. Bun and Deno have had that API for a long time already. But until
 // we're ready to drop older versions of Node, we need this library to support it.
-cosole.log('main' in import.meta);
+console.log('main' in import.meta);
 if (esMain(import.meta)) {
   try {
     await main(process.argv.slice(2));
